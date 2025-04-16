4822ce409d3ec04873efa9604b9b1d343f0753f0
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-04-16 20:43:48 +0530

    contact ui done

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  faHome= faHome;
 }
